7155466a2ac832c5422d4a907bd58561345f55be
BKK <787885589@qq.com>
2023-09-01 17:12:49 +0800

    启用了CPBR_ENABLE_ASSERTS

--- Capybara/src/Capybara/Core.h
+++ Capybara/src/Capybara/Core.h
@@ -3,21 +3,25 @@
 // 应该是在预处理器设置里define CPBR_PLATFORM_WINDOWS或CPBR_BUILD_DLL
 // 目的是简化app和engine的export/import操作
 #ifdef CPBR_PLATFORM_WINDOWS
-#ifdef CPBR_BUILD_DLL
-#define CAPYBARA_API __declspec(dllexport)
+    #ifdef CPBR_BUILD_DLL
+        #define CAPYBARA_API __declspec(dllexport)
+    #else
+        #define CAPYBARA_API __declspec(dllimport)
+    #endif
 #else
-#define CAPYBARA_API __declspec(dllimport)
+    #error Capybara only supports windows
 #endif
-#else
-#error Capybara only supports windows
+
+#ifdef CPBR_DEGUG
+    #define CPBR_ENABLE_ASSERTS
 #endif
 
 #ifdef CPBR_ENABLE_ASSERTS
-#define CPBR_ASSERT(x, ...) { if (!(x)) {CPBR_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-#define CPBR_CORE_ASSERT(x, ...) { if (!(x)) {CPBR_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+    #define CPBR_ASSERT(x, ...) { if (!(x)) {CPBR_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+    #define CPBR_CORE_ASSERT(x, ...) { if (!(x)) {CPBR_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
-#define CPBR_ASSERT(x, ...)
-#define CPBR_CORE_ASSERT(x, ...) 
+    #define CPBR_ASSERT(x, ...)
+    #define CPBR_CORE_ASSERT(x, ...) 
 #endif
 
 #define BIT(x) 1 << x
