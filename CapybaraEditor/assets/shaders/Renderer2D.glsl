d59500b71963eeeb0ec9dc68ec52efa203c2cc31
BKK <787885589@qq.com>
2023-10-05 02:10:22 +0800

    fix shader bugs

--- CapybaraEditor/assets/shaders/Renderer2D.glsl
+++ CapybaraEditor/assets/shaders/Renderer2D.glsl
@@ -18,11 +18,11 @@ out float v_TilingFactor;
 
 void main()
 {
-        v_Color = a_Color;
-        v_TexCoord = a_TexCoord;
-        v_TexIndex = a_TexIndex;
-        v_TilingFactor = a_TilingFactor;
-        gl_Position = u_ViewProjection * (a_Position, 1.0);
+    v_Color = a_Color;
+    v_TexCoord = a_TexCoord;
+    v_TexIndex = a_TexIndex;
+    v_TilingFactor = a_TilingFactor;
+    gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 }
 
 // #type fragment
@@ -39,5 +39,5 @@ uniform sampler2D u_Textures[32];
 
 void main()
 {
-        color = texture(u_Texture[int(v_TexIndex)], v_TexCoord * v_TilingFactor) * v_Color;
+    color = texture(u_Textures[int(v_TexIndex)], v_TexCoord * v_TilingFactor) * v_Color;
 }
--- CapybaraEditor/assets/shaders/Renderer2D_Line.glsl
+++ CapybaraEditor/assets/shaders/Renderer2D_Line.glsl
@@ -13,7 +13,7 @@ out vec4 v_Color;
 void main()
 {
     v_Color = a_Color;
-    gl_Position = u_ViewProjection * (a_Position, 1.0);
+    gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 }
 
 // #type fragment
